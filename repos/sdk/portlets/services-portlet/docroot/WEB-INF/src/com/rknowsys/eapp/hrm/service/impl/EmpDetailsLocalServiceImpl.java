b3cdc7684e5011e48b057bfed4cd169edf619fe4
laxminarayanav <laxminarayana.v@rknowsys.com>
2014-10-13 18:42:37 +0530

    Written search query using stringbuffer.

--- repos/sdk/portlets/services-portlet/docroot/WEB-INF/src/com/rknowsys/eapp/hrm/service/impl/EmpDetailsLocalServiceImpl.java
+++ repos/sdk/portlets/services-portlet/docroot/WEB-INF/src/com/rknowsys/eapp/hrm/service/impl/EmpDetailsLocalServiceImpl.java
@@ -16,16 +16,8 @@ package com.rknowsys.eapp.hrm.service.impl;
 
 import java.util.List;
 
-import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.Junction;
-import com.liferay.portal.kernel.dao.orm.Property;
-import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.Validator;
 import com.rknowsys.eapp.hrm.model.EmpDetails;
-import com.rknowsys.eapp.hrm.service.EmpDetailsLocalServiceUtil;
 import com.rknowsys.eapp.hrm.service.base.EmpDetailsLocalServiceBaseImpl;
 import com.rknowsys.eapp.hrm.service.persistence.EmpDetailsFinderUtil;
 
@@ -49,81 +41,10 @@ public class EmpDetailsLocalServiceImpl extends EmpDetailsLocalServiceBaseImpl {
 	 *
 	 * Never reference this interface directly. Always use {@link com.rknowsys.eapp.hrm.service.EmpDetailsLocalServiceUtil} to access the emp details local service.
 	 */
-	public List<EmpDetails> findByEmpDetails(int begin, int end)
+	public List<EmpDetails> findEmpDetails(String empname,String empid,String empstatus,String supervisorname,String jobtitle,String subunit,int begin, int end)
 			throws SystemException {
 		      System.out.println("in empdetailslocalserviceimpl.....");
 
-			    return EmpDetailsFinderUtil.findByEmpDetails(begin, end);
+			    return EmpDetailsFinderUtil.findEmpDetails(empname,empid,empstatus,supervisorname,jobtitle,subunit,begin, end);
 		}
-	
-	/**
-	 * This method returns employee list based on the given search parameters.
-	 * 
-	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public List getSearchEmployees(String empname,String empid, String empstatus,String supervisorname,String jobtitle,String subunit,boolean andSearch, int start, int end)
-	        throws SystemException
-	    {
-	        DynamicQuery dynamicQuery = buildEmployeeDynamicQuery(empname,empid,empstatus,supervisorname,jobtitle,subunit,andSearch);
-	        return EmpDetailsLocalServiceUtil.dynamicQuery(dynamicQuery, start, end);//  StudentLocalServiceUtil.dynamicQuery(dynamicQuery, start, end, orderByComparator);
-	    }
-		/**
-		 * This method returns employee list count based on the given search parameters.
-		 */
-	    public int getSearchEmployeesCount(String empname,String empid, String empstatus,String supervisorname,String jobtitle,String subunit, boolean andSearch)
-	            throws SystemException
-	    {
-	    	DynamicQuery dynamicQuery = buildEmployeeDynamicQuery(empname,empid,empstatus,supervisorname,jobtitle,subunit,andSearch);
-	        return (int)EmpDetailsLocalServiceUtil.dynamicQueryCount(dynamicQuery);
-	    }
-
-	    protected DynamicQuery buildEmployeeDynamicQuery(String empname,String empid, String empstatus,String supervisorname,String jobtitle,String subunit,boolean andSearch)
-	    {
-	    	Junction junction = null;
-	        if(andSearch)
-	            junction = RestrictionsFactoryUtil.conjunction();
-	        else
-	            junction = RestrictionsFactoryUtil.disjunction();
-	        
-	        if(Validator.isNotNull(empname))
-	        {
-	            Property property = PropertyFactoryUtil.forName("firstName");
-	            String value = (new StringBuilder("%")).append(empname).append("%").toString();
-	            System.out.println("value == " +value);
-	            junction.add(property.like(value));
-	        }
-	        if(Validator.isNotNull(empid))
-	        {
-	            Property property = PropertyFactoryUtil.forName("employeeNo");
-	            String value = (new StringBuilder("%")).append(empid).append("%").toString();
-	            junction.add(property.like(value));
-	        }
-	       
-	        if(Validator.isNotNull(empstatus))
-	        {
-	            Property property = PropertyFactoryUtil.forName("employmentstatus");
-	            String value = (new StringBuilder("%")).append(empstatus).append("%").toString();
-	            junction.add(property.like(value));
-	        }
-	        if(Validator.isNotNull(supervisorname))
-	        {
-	            Property property = PropertyFactoryUtil.forName("supervisor");
-	            String value = (new StringBuilder("%")).append(supervisorname).append("%").toString();
-	            junction.add(property.like(value));
-	        }
-	        if(Validator.isNotNull(jobtitle))
-	        {
-	            Property property = PropertyFactoryUtil.forName("title");
-	            String value = (new StringBuilder("%")).append(jobtitle).append("%").toString();
-	            junction.add(property.like(value));
-	        }
-	        if(Validator.isNotNull(subunit))
-	        {
-	            Property property = PropertyFactoryUtil.forName("name");
-	            String value = (new StringBuilder("%")).append(subunit).append("%").toString();
-	            junction.add(property.like(value));
-	        }
-	        DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(EmpDetails.class, getClassLoader());
-	        return dynamicQuery.add(junction);
-	    }
 }
