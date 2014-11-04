ddb58c2f954b7c67135c8287d4ea99a0ada2cb65
Geeta Arora <geeta.a.arora@gmail.com>
2014-11-04 18:55:36 +0530

    Issue # 69: Leave Types This checkin includes: 1. Leave Types 2. Leave Rule 3. Leave Restriction 4. Leave Type Applicability 5. Leave Carry Forward

---
+++ repos/sdk/portlets/services-portlet/docroot/WEB-INF/src/com/rknowsys/eapp/hrm/service/impl/LeaveRuleLocalServiceImpl.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.rknowsys.eapp.hrm.service.impl;
+
+import com.rknowsys.eapp.hrm.service.base.LeaveRuleLocalServiceBaseImpl;
+
+/**
+ * The implementation of the leave rule local service.
+ * 
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link com.rknowsys.eapp.hrm.service.LeaveRuleLocalService} interface.
+ * 
+ * <p>
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
+ * </p>
+ * 
+ * @author rknowsys
+ * @see com.rknowsys.eapp.hrm.service.base.LeaveRuleLocalServiceBaseImpl
+ * @see com.rknowsys.eapp.hrm.service.LeaveRuleLocalServiceUtil
+ */
+public class LeaveRuleLocalServiceImpl extends LeaveRuleLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 * 
+	 * Never reference this interface directly. Always use {@link
+	 * com.rknowsys.eapp.hrm.service.LeaveRuleLocalServiceUtil} to access the
+	 * leave rule local service.
+	 */
+
+	public java.util.List<com.rknowsys.eapp.hrm.model.LeaveRule> findByLeaveTypeId(
+			long leaveTypeId)
+			throws com.liferay.portal.kernel.exception.SystemException {
+
+		return leaveRulePersistence.findByLeaveTypeId(leaveTypeId);
+	}
+
+}
